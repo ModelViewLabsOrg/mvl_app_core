6f4d7a22225f5faa0c6551d5bd558f6a99c9a766
Gabriel Rozendo <gabriel.rozendo@gmail.com>
2025-03-26 12:37:13 +0000

    handle errors

--- lib/utils/show_exception_alert_dialog.dart
+++ lib/utils/show_exception_alert_dialog.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mvl_app_core/widgets/app_toast_message.dart';
 
-String genericErrorMessage = 'Something went wrong. Please try again later.';
-
-String? Function(dynamic exception) handleException = (_) => null;
+String? Function(BuildContext context, dynamic exception) handleException = (_, _) => null;
 
 void showException({required BuildContext context, required dynamic exception}) {
-  AppToastMessages(_message(exception), isError: true).show(context);
+  AppToastMessages(_message(context, exception), isError: true).show(context);
 }
 
-String _message(dynamic exception) {
-  final message = handleException(exception);
+String _message(BuildContext context, dynamic exception) {
+  final message = handleException(context, exception);
   if (message != null) return message;
 
-  if (exception is PlatformException) return exception.message ?? genericErrorMessage;
+  if (exception is PlatformException) {
+    final message = exception.message;
+    if (message != null && message.isNotEmpty) return message;
+  }
 
-  //  exception.toString();
-  return genericErrorMessage;
+  return 'Houve um erro aqui, tente novamente mais tarde!';
 }
