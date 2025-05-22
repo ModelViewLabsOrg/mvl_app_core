7ea1b2d8a9cfdedd6deebadae332badda47c9018
Gabriel Rozendo <gabriel.rozendo@gmail.com>
2025-05-22 22:37:43 +0100

    valueGuard

--- lib/widgets/async_value_ui.dart
+++ lib/widgets/async_value_ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mvl_app_core/mvl_app_core.dart';
 import 'package:mvl_app_core/utils/show_exception_alert_dialog.dart';
 
 extension AsyncValueUI on AsyncValue<dynamic> {
@@ -10,4 +11,27 @@ extension AsyncValueUI on AsyncValue<dynamic> {
       showException(context: context, exception: error);
     }
   }
+}
+
+Future<AsyncValue<T>> valueGuard<T>(
+  Future<T> Function() future, {
+  required String method,
+  AsyncValue<T>? withLoading,
+}) {
+  if (withLoading != null) {
+    withLoading = AsyncLoading();
+  }
+
+  return AsyncValue.guard(
+    future,
+    (e) {
+      AppLogger.I().error(
+        method,
+        e,
+        StackTrace.current,
+        {'method': method, 'error': e.toString()},
+      );
+      return true;
+    },
+  );
 }
