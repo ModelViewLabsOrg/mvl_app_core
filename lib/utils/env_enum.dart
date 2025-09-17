763180f91b36d811c2ff9e55cb06f15b77f7b50a
Gabriel Rozendo <gabriel.rozendo@gmail.com>
2025-09-17 16:42:50 +0200

    Add 'dev' environment to EnvEnum and rename Flavor class to AppFlavor

--- lib/utils/env_enum.dart
+++ lib/utils/env_enum.dart
@@ -1,11 +1,11 @@
-enum EnvEnum { prd, uat }
+enum EnvEnum { dev, uat, prd }
 
 extension EnvEnumExt on EnvEnum {
-  // bool get isDev => this == EnvEnum.dev;
+  bool get isDev => this == EnvEnum.dev;
   bool get isUat => this == EnvEnum.uat;
   bool get isPrd => this == EnvEnum.prd;
 }
 
-class Flavor {
-  static EnvEnum appEnv = EnvEnum.prd;
+class AppFlavor {
+  static EnvEnum env = EnvEnum.prd;
 }
