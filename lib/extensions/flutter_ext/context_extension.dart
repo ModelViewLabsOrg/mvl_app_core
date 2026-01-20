b36b2ae63334813a5a5f8a4216b4619bbe87453a
Gabriel Rozendo <gabriel.rozendo@gmail.com>
2026-01-20 12:22:46 -0300

    build context ext

--- lib/extensions/flutter_ext/context_extension.dart
+++ lib/extensions/flutter_ext/context_extension.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mvl_app_core/utils/show_exception_alert_dialog.dart';
+import 'package:mvl_app_core/widgets/app_toast_message.dart';
 
 extension ContextExtensions on BuildContext {
   ThemeData get theme => Theme.of(this);
@@ -19,7 +21,43 @@ extension ContextExtensions on BuildContext {
   /// Returns same as MediaQuery.of(context).height
   double get heightPx => sizePx.height;
 
-  void unfocus() => FocusScope.of(this).unfocus();
+  double get textScaleFactor => mq.textScaler.scale(1);
+  bool get isTextBig => textScaleFactor > 1.1;
+
+  double get deviceScaleFactor {
+    return switch (widthPx) {
+      <= 400 => .85,
+      >= 600 => 1.25,
+      _ => 1,
+    };
+  }
+
+  void unfocus() {
+    try {
+      FocusScope.of(this).unfocus();
+      final FocusScopeNode currentScope = FocusScope.of(this);
+      if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      }
+    } catch (_) {}
+  }
+
+  void requestFocus(FocusNode node) {
+    FocusScope.of(this).requestFocus(node);
+  }
 
   void dismissKeyboard() => FocusManager.instance.primaryFocus?.unfocus();
+
+  void showToastError(Object exception, {StackTrace? stackTrace, String? method}) {
+    showException(
+      context: this,
+      method: method ?? 'showToastError',
+      exception: exception,
+      stackTrace: stackTrace,
+    );
+  }
+
+  void showToast(String message) {
+    AppToastMessages(message).show(this);
+  }
 }
